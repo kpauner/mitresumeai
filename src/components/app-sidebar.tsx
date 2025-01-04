19094276f0d6853c1d20298c2391b4b3b1f93689
kpauner <82957241+kpauner@users.noreply.github.com>
2025-01-04 19:44:46 +0100

    feat: app sidebar

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,93 @@
+import * as React from "react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { ModeToggle } from "./mode-toggle";
+import { Command, LifeBuoy, Send } from "lucide-react";
+import { NavUser } from "@/features/auth/components/nav-user";
+import { NavSecondary } from "./nav-secondary";
+
+// This is sample data.
+const data = {
+  user: {
+    name: "John Doe",
+    email: "john.doe@example.com",
+    avatar: "https://github.com/shadcn.png",
+  },
+  navMain: [
+    {
+      title: "Erfaring",
+      url: "#",
+      icon: LifeBuoy,
+    },
+    {
+      title: "Uddannelse",
+      url: "#",
+      icon: Send,
+    },
+  ],
+  navSecondary: [
+    {
+      title: "Ansøgninger",
+      url: "#",
+      icon: LifeBuoy,
+    },
+    {
+      title: "CV",
+      url: "#",
+      icon: Send,
+    },
+  ],
+  navFooter: [
+    {
+      title: "Support",
+      url: "#",
+      icon: LifeBuoy,
+    },
+    {
+      title: "Feedback",
+      url: "#",
+      icon: Send,
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <a href="#">
+                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                  <Command className="size-4" />
+                </div>
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-semibold">Acme Inc</span>
+                  <span className="truncate text-xs">Enterprise</span>
+                </div>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+          <ModeToggle />
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavSecondary title="Profil" items={data.navMain} className="" />
+        <NavSecondary title="Assistent" items={data.navSecondary} />
+        <NavSecondary items={data.navFooter} className="mt-auto" />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
---
+++ src/components/nav-secondary.tsx
@@ -0,0 +1,44 @@
+import * as React from "react";
+import { type LucideIcon } from "lucide-react";
+
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+export function NavSecondary({
+  title,
+  items,
+  ...props
+}: {
+  title?: string;
+  items: {
+    title: string;
+    url: string;
+    icon: LucideIcon;
+  }[];
+} & React.ComponentPropsWithoutRef<typeof SidebarGroup>) {
+  return (
+    <SidebarGroup {...props}>
+      <SidebarGroupContent>
+        {title && <SidebarGroupLabel>{title}</SidebarGroupLabel>}
+        <SidebarMenu>
+          {items.map((item) => (
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton asChild size="sm">
+                <a href={item.url}>
+                  <item.icon />
+                  <span>{item.title}</span>
+                </a>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+  );
+}
